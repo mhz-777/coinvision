336c7653d7018e6099c1c30ba000e21d2d7761a6
Mustafa <mhassanzadeh777@gmail.com>
2023-02-06 16:00:37 -0700

    Lifted state up to parent, added API test function, added API test results to footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import './App.css'
 import Search from './components/Search';
+import Results from './components/Results';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  // user input state
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  // api data
+  const [apiData, setApiData] = useState<string>('');
+
+  // passdown function for search
+  const handleSearchTermChange = (value:string) => {
+    setSearchTerm(value);
+  }
+
+  // ping api to check status on initial load
+  useEffect (()=>{
+    const pingAPI = async () => {
+      const response = await fetch('https://api.coingecko.com/api/v3/ping');
+      if(!response.ok) {
+        setApiData('down');
+      }
+    };
+    pingAPI();
+  }, [])
 
   return (
     <div className="App">
@@ -10,8 +34,25 @@ function App() {
           <button className="settings-btn"></button>
       </header>
       <main>
-        <Search />
+        <Search 
+          onSearchTermChange={handleSearchTermChange}   
+        />
+        <Results
+          searchTerm={searchTerm}
+        />
+        <footer className="main-sec-footer">
+          <p className="api-status-header">api status:</p>
+          {apiData != 'down' &&
+            <div className="api-operational"></div>
+          }
+          {apiData == 'down' &&
+            <div className="api-unoperational"></div>
+          }
+        </footer>
       </main>
+      <footer className="site-footer">
+        <a href="https://github.com/mhz-777/coinvision" target='_blank' className="footer-github-link"></a>
+      </footer>
     </div>
   )
 }
