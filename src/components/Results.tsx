c0181838471a452850e8e8c616d7fa8dceb00378
Mustafa <mhassanzadeh777@gmail.com>
2023-02-06 22:43:49 -0700

    added function to get data from api based off user query

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './Results.css';
 
 interface resultProps {
@@ -5,6 +6,25 @@ interface resultProps {
 }
 
 const Results: React.FC<resultProps> = ({searchTerm}) => {
+
+    // url based off user query
+    let apiURL = `https://api.coingecko.com/api/v3/coins/${searchTerm}?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`;
+
+    // function to get data from user query
+    const getCoinData = async () => {
+        const response = await fetch(apiURL);
+        const coinData = await response.json();
+        
+    }
+
+    // get data when searchTerm is updated with value
+    useEffect(()=> {
+        if(searchTerm != '') {
+            getCoinData();
+        }
+    }, [searchTerm])
+
+    // render results, otherwise fallback to placeholder
     if(searchTerm) {
         return (
             <section className="results-section">
