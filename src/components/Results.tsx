27820796516ddb07950563e9397a699f1706fa61
Mustafa <mhassanzadeh777@gmail.com>
2023-02-07 14:50:20 -0700

    Added state to store received coin attributes, implemented try catch for getCoinData function, made user query lowercase in API url, added conditional render for invalid coin input, displayed retrieved data in header

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './Results.css';
+import errorSVG from '../assets/images/errorsvgtriangle.svg';
 
 interface resultProps {
     searchTerm: string;
@@ -7,14 +8,43 @@ interface resultProps {
 
 const Results: React.FC<resultProps> = ({searchTerm}) => {
 
-    // url based off user query
-    let apiURL = `https://api.coingecko.com/api/v3/coins/${searchTerm}?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`;
+    // state to manage coin attributes
+    const [coinAttributes, setCoinAttributes] = useState({
+        coinName: '',
+        coinImage: '',
+        coinPrice: '',
+        creationDate: '',
+        dailyChange: '',
+        dailyVolume: ''
+    });
 
-    // function to get data from user query
+    // state to handle error in query
+    const [validSearch, setValidSearch] = useState<boolean>(false);
+    const [error, setError] = useState<boolean>(false);
+
+    // url based off user query
+    let apiURL = `https://api.coingecko.com/api/v3/coins/${searchTerm.toLowerCase()}?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`;
+
+    // function to get and set data from user query
     const getCoinData = async () => {
-        const response = await fetch(apiURL);
-        const coinData = await response.json();
-        
+        try {
+            const response = await fetch(apiURL);
+            const coinData = await response.json();
+            setCoinAttributes({
+                ...coinAttributes,
+                coinName: coinData.name,
+                coinImage: coinData.image.small,
+                coinPrice: coinData.market_data.current_price.cad,
+                creationDate: coinData.genesis_date,
+                dailyChange: coinData.market_data.price_change_percentage_24h,
+                dailyVolume: coinData.market_data.total_volume.cad
+            });
+            setValidSearch(true);
+        }catch (error) {
+            setError(true);
+            setValidSearch(false);
+        }
+
     }
 
     // get data when searchTerm is updated with value
@@ -25,19 +55,43 @@ const Results: React.FC<resultProps> = ({searchTerm}) => {
     }, [searchTerm])
 
     // render results, otherwise fallback to placeholder
-    if(searchTerm) {
+    if(searchTerm && validSearch === true) {
         return (
             <section className="results-section">
-                <h1>{searchTerm}</h1>
+                <header className="results-section-header">
+                    <section className="header-title-image-group">
+                        <img src={coinAttributes.coinImage} alt="coin" className="coin-img" />
+                        <h1 className="coin-header">{coinAttributes.coinName}</h1>
+                    </section>
+                    <h3 className="coin-creation-header">Created: {coinAttributes.creationDate}</h3>
+                </header>
             </section>
-        )
-    } else {
+        );
+    }else if(error === true){
+        return (
+            <section className="results-section-error">
+                <section className="error-message-container">
+                    <img src={errorSVG} alt="error triangle" className='errorsvg-img'/>
+                    <h1 className="error-message-heading">Coin not found.</h1>
+                    <p className="error-message-desc">
+                        Click 
+                        <a href='https://docs.google.com/spreadsheets/d/1wTTuxXt8n9q7C4NDXqQpI3wpKu1_5bGVmP9Xz0XGSyU/edit#gid=0' target='_blank' className='error-message-link'> here </a> 
+                        to be redirected to the API coins list, and then try again.
+                    </p>
+                    <p className="error-message-desc">
+                        Be sure to enter the ID (API ID) rather then the symbol or name.
+                    </p>
+                </section>
+            </section>
+        );
+    }else {
         return (
             <section className="results-section-placeholder">
                 <h1 className="results-placeholder-heading">Enter a coin above to retrieve data.</h1>
             </section>
-        )
+        );
     }
+    
 }
 
 export default Results;
